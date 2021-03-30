3edf2c40c5f1b493687395f74775e002d94bf627
karin valcanover <valcanoverkarin@outlook.it>
2021-03-30 17:28:30 +0200

    inizio js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Il programma dovrà consentire di calcolare il prezzo del panino 
+//selezionando o deselezionando gli ingredienti proposti.
+
+// funzionalità del bottone calculate
+var calculateButton = document.getElementById('calculate_button');
+calculateButton.addEventListener ( 'click', function() {
+
+}
